0a0757c012f2a8e48e10f028b86503f6eef39295
Gabriel-Montes <a.22308051281276@cbtis128.edu.mx>
2024-09-06 09:34:11 -0600

    Add files via upload

---
+++ miscadenas.py
@@ -0,0 +1,30 @@
+"""TRabajando con cadenas
+"""
+''' tambien sive como comentario xd                 
+'''
+
+name="Panda"
+print(name[1])
+print(name)
+
+for x in "PANDA ":
+    print(x)
+
+mn=["a","b","c"]
+for a in mn:
+    print(a)
+    
+print("otro ejemplo")
+a = "Gabriel Montes"
+print(a[2:5])
+
+print("MAYUSCULAS")
+a = "Gabriel Montes"
+print(a.upper())
+
+print("CONCATENAR")
+a = "Gabriel "
+b = " Montes"
+c = a + b
+print(c)
+# python miscadenas.py
---
+++ mivariables.py
@@ -0,0 +1,17 @@
+import math
+
+edad=17 #edad vale 7
+nombre="Gabriel Montes"
+print("tu edad es ",edad)
+print("Tu nombre es " ,nombre)
+print("-Tipos de variables-")
+x = str(4)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x)
+print(y)
+print(z) 
+
+print("*-funciones matematicas-*")
+x = math.sqrt(64)
+print(x)
